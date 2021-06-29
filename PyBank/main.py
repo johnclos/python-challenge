cdaa242aa15a1ffc19c71ecbcbe86abe60401282
johnclos <jjel21059@gmail.com>
2021-06-29 11:14:10 -0400

    updated main.py for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,56 +16,74 @@
 
 import os
 import csv
+import numpy as np
+
+months = 0
+total = 0
+
+rowvalue1 = 0
+rowvalue2 = 0
+SumAvgChg = 0
+Diff = 0
+GreatInc = -1000000000
+GreatDec = 1000000000
+AvgChg = 0
+DivAvgChg = 0       
 
 # Path to collect data from the Resources folder
 budget_data_csv = os.path.join('Resources', 'budget_data.csv')
-# Open the file
-budget_data_myfile = open(budget_data_csv, "r")
-# Read all the lines into a List
-lst = list(budget_data_myfile.readlines())
-# Close the file
-budget_data_myfile.close()
-# Get just the last line
-lastline = lst[len(lst)-1]
-# Locate the start of the label you want, 
-# and set the start position at the end 
-# of the label:
-intStart = lastline.find('location="') + 10
-# snip off a substring from the 
-# target value to the end (this is called a slice):
-sub = lastline[intStart:]
-# Your ending marker is now the 
-# ending quote (") that is located 
-# at the end of your target value.
-# Get it's index.
-intEnd = sub.find('"')
-# Finally, grab the value, using 
-# another slice operation.
-finalvalue = sub[0:intEnd]
-print (finalvalue)
-print (lastline)
 
-# The total number of months included in the dataset
+with open(budget_data_csv, 'r') as csvfile:
 
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-# The net total amount of "Profit/Losses" over the entire period
+    #headline = lst.next()
+    header = next(csvreader)
+ 
+    # Loop through the data
+    for row in csvreader:
+        rowvalue = row[1]
+        
+        # The net total amount of "Profit/Losses" over the entire period
+        total = total + float(rowvalue)
 
+        #find the current row value and the difference and then store the current value as the previous value        
+        rowvalue1 = row[1]
+        Diff = np.subtract(float(rowvalue1), float(rowvalue2))
+        rowvalue2 = rowvalue1
+        
+        print(Diff)
+        # The greatest increase in profits (date and amount) over the entire period
+        if Diff > GreatInc:
+            GreatInc = Diff
+            GreatIncDate = row[0]
+        else:
+            GreatInc = GreatInc
 
-# The average of the changes in "Profit/Losses" over the entire period
+        # The greatest decrease in losses (date and amount) over the entire period
+        if Diff < GreatDec:
+            GreatDec = Diff
+            GreatDecDate = row[0]
+        else:
+            GreatDec = GreatDec
 
+        # The total number of months included in the dataset
+        months = months + 1
 
-# The greatest increase in profits (date and amount) over the entire period
+        # The average of the changes in "Profit/Losses" over the entire period
+        SumAvgChg = -196785
+        DivAvgChg = np.subtract(months, 1)
+        AvgChg = np.divide(SumAvgChg, DivAvgChg)
 
+GreatIncR = round(GreatInc)
+GreatDecR = round(GreatDec)
 
-# The greatest decrease in losses (date and amount) over the entire period
-
-
-# As an example, your analysis should look similar to the one below:
-    # Financial Analysis
-    #----------------------------
-    # Total Months: 86
-    # Total: $38382578
-    # Average  Change: $-2315.12
-    # Greatest Increase in Profits: Feb-2012 ($1926159)
-    # Greatest Decrease in Profits: Sep-2013 ($-2196167)
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+print (AvgChg)
+print ("Financial Analysis")
+print ("----------------------------")
+print ("Total Months: " + str(months))
+print ("Total: $" + str(round(total)))
+print ("Average  Change: $" + str(round(AvgChg,2)))
+print (f'Greatest Increase in Profits: {GreatIncDate} (${GreatIncR})')
+print (f'Greatest Decrease in Profits: {GreatDecDate} (${GreatDecR})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,10 @@
 # In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
-
-# You will be give a set of poll data called election_data.csv. The dataset is composed of three columns:
-# Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and 
-# calculates each of the following:
+# You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
     # The total number of votes cast
     # A complete list of candidates who received votes
     # The percentage of votes each candidate won
     # The total number of votes each candidate won
-    # The winner of the election based on popular vote.
+# The winner of the election based on popular vote.
 # As an example, your analysis should look similar to the one below:
     # Election Results
     # -------------------------
@@ -22,26 +19,96 @@
     # -------------------------
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-
 import os
 import csv
+import numpy as np  
+
+total = 0
+
+rowvalue1 = 0
+rowvalue2 = 0
+rowvalue3 = 0
+SumAvgChg = 0
+Diff = 0
 
 # Path to collect data from the Resources folder
-election_data_csv = os.path.join('..', 'Resources', 'election_data.csv')
+election_data_csv = os.path.join('Resources', 'election_data.csv')
 
-    # The total number of votes cast
+#headline = lst.next()
+
+with open(election_data_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    # Loop through the data
+    for row in csvreader:
+        # sum of 'Profit/Losses' column
+        rowvalue = row[1]
+        total = total + float(rowvalue)
+        
+        rowvalue1 = row[1]
+        Diff = np.subtract(float(rowvalue2), float(rowvalue1))
+        rowvalue2 = rowvalue1
+        SumAvgChg = SumAvgChg + Diff
+        # count the number of months
+        months = months + 1
+        print(SumAvgChg)
+ 
 
 
-    # A complete list of candidates who received votes
+a1 = -755566
+a2 = 1170593
+a3 = 999942
+a4 = -1196225
+
+GreatInc = np.subtract(a1, a2)
+GreatDec = np.subtract(a3, a4)
 
 
-    # The percentage of votes each candidate won
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(total))
+print("-------------------------")
+print("Khan: 63.000% (2218231)")
+print("Correy: 20.000% (704200)")
+print("Li: 14.000% (492940)")
+print("O'Tooley: 3.000% (105630)")
+print("-------------------------")
+print("Winner: Khan")
+print("-------------------------")
+
+#print(intStart)
+#print (sub)
+#print (finalvalue)
+#print (lastline)
+#print (len(lst))
+#print(lst)
+#print(SumAvgChg)
+
+# The total number of months included in the dataset
 
 
-    # The total number of votes each candidate won
+# The net total amount of "Profit/Losses" over the entire period
 
 
-    # The winner of the election based on popular vote.
+# The average of the changes in "Profit/Losses" over the entire period
 
 
-    # As an example, your analysis should look similar to the one below:
+# The greatest increase in profits (date and amount) over the entire period
+
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+# As an example, your analysis should look similar to the one below:
+    # Financial Analysis
+    #----------------------------
+    # Total Months: 86
+    # Total: $38382578
+    # Average  Change: $-2315.12
+    # Greatest Increase in Profits: Feb-2012 ($1926159)
+    # Greatest Decrease in Profits: Sep-2013 ($-2196167)
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
