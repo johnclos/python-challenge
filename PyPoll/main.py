145f48c2cba7d4068f0f281fdedcfde13ce4c7bc
johnclos <jjel21059@gmail.com>
2021-06-30 12:01:33 -0400

    updated main.py in PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -23,10 +23,13 @@
 
 import os
 import csv
-import numpy as np  
+import pandas as pd  
 
+r = 0
+candidate_list = []
+cand_lst_len = 0
 total = 0
-
+candidate1 = 'a'
 # Path to collect data from the Resources folder
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
@@ -37,14 +40,34 @@ with open(election_data_csv, 'r') as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    # skip header row
     header = next(csvreader)
 
+    # get the first candidate
+    
     # Loop through the data
     for row in csvreader:
         # The total number of votes cast
-        total = np.add(total, 1)
+        total = total + 1
 
-print("Election Results")
+        # A complete list of candidates who received votes
+        candidate1 = row[2]
+        if candidate1 not in candidate_list:
+            candidate_list.append(row[2])
+        else:
+            r = 1
+        
+        cand_lst_len = len(candidate_list)
+        # The percentage of votes each candidate won
+
+
+        # The total number of votes each candidate won
+
+
+        # The winner of the election based on popular vote.
+
+
+print ("Election Results")
 print("-------------------------")
 print("Total Votes: " + str(total))
 print("-------------------------")
@@ -55,3 +78,24 @@ print("O'Tooley: 3.000% (105630)")
 print("-------------------------")
 print("Winner: Khan")
 print("-------------------------")
+
+
+print(candidate_list)
+print(cand_lst_len)
+
+# create the text file
+PyPollSummary = ["Election Results",
+    "-------------------------",
+    "-------------------------"
+    ]
+print (PyPollSummary)
+
+open("PyPollSummary.txt", "w")
+# save the data to the text file
+with open("PyPollSummary.txt", "w") as txt_file:
+    for line in PyPollSummary:
+        txt_file.write(line + "\n")
+
+
+# close the text file
+#PyPollSummary.close()
